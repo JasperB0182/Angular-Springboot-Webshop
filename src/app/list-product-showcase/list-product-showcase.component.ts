97638e7f94777788d6271ed2840bd282034a5bf9
Jasper batenburg <1155772@student.hsleiden.nl>
2025-03-14 16:19:45 +0100

    Backend completely connected to list-product-showcase.component.ts

--- src/app/list-product-showcase/list-product-showcase.component.ts
+++ src/app/list-product-showcase/list-product-showcase.component.ts
@@ -1,6 +1,5 @@
 import {Component, DestroyRef, inject, Input} from '@angular/core';
 import {ProductShowcaseComponent} from "../product-showcase/product-showcase.component";
-import {DUMMY_PRODUCTS} from '../DUMMY_PRODUCTS';
 import {HttpClient, provideHttpClient} from '@angular/common/http';
 import {Product} from '../models/product-model';
 
@@ -15,12 +14,13 @@ import {Product} from '../models/product-model';
 export class ListProductShowcaseComponent {
   private httpClient = inject(HttpClient)
   private destroyRef = inject(DestroyRef)
-  protected readonly DummyProducts = DUMMY_PRODUCTS;
+  DummyProducts: any;
 
 
   ngOnInit() {
     const subscription = this.httpClient.get<{Product : Product}>("http://localhost:8080/api/products").subscribe({
       next: (resData) => {
+        this.DummyProducts = resData;
         console.log(resData)
       }
     });
