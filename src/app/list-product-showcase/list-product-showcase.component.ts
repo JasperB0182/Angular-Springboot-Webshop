37bd952a3227e09d2e84d30c9d69acc7c772bed6
Jasper batenburg <1155772@student.hsleiden.nl>
2025-03-14 16:12:26 +0100

    Connected backend, only need to load from it

--- src/app/list-product-showcase/list-product-showcase.component.ts
+++ src/app/list-product-showcase/list-product-showcase.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, DestroyRef, inject, Input} from '@angular/core';
 import {ProductShowcaseComponent} from "../product-showcase/product-showcase.component";
 import {DUMMY_PRODUCTS} from '../DUMMY_PRODUCTS';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {Product} from '../models/product-model';
 
 @Component({
   selector: 'app-list-product-showcase',
@@ -8,8 +10,29 @@ import {DUMMY_PRODUCTS} from '../DUMMY_PRODUCTS';
     ProductShowcaseComponent
   ],
   templateUrl: './list-product-showcase.component.html',
-  styleUrl: './list-product-showcase.component.scss'
+  styleUrl: './list-product-showcase.component.scss',
 })
 export class ListProductShowcaseComponent {
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
   protected readonly DummyProducts = DUMMY_PRODUCTS;
+
+
+  ngOnInit() {
+    const subscription = this.httpClient.get<{Product : Product}>("http://localhost:8080/api/products").subscribe({
+      next: (resData) => {
+        console.log(resData)
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+
+
+  }
+
+
+
+
 }
