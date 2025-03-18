9916a2bfead1a03e1d89cf7cfe35ee232803db27
Jasper Batenburg <s1155772@student.hsleiden.nl>
2025-03-18 10:37:05 +0100

    Refactored homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './header/header.component';
-import {WelcomeScreenComponent} from './welcome-screen/welcome-screen.component';
-import {ProductSectionComponent} from './product-section/product-section.component';
+import {HomepageComponent} from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, WelcomeScreenComponent, ProductSectionComponent],
+  imports: [RouterOutlet, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
